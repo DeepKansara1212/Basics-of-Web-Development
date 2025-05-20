a5a7cfa25e248745d5e122b2133e18980d986994
Deep Kansara <deepkansara1212@gmail.com>
2025-05-20 13:20:34 +0530

    Javascript

---
+++ Javascript/02_Variables/index.js
@@ -0,0 +1,25 @@
+// fruits = "mango"; 
+// fruits = "banana";
+
+number = 5
+age = 20
+
+// let var const
+
+// Variable Declaration
+// console.log(fruits);
+let fruits = "apple";
+
+
+// let fruits;
+// fruits = "apple";
+
+
+console.log(cars);
+var cars = "BMW";
+// var cars = "Mercedes"; // redeclaration is allowed
+
+const pi = 3.14;
+console.log("hello world");
+
+console.log(pi);

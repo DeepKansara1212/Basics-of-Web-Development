bbe608643a68a186be36b3fccc9db3e95304033b
Deep Kansara <deepkansara1212@gmail.com>
2025-05-30 13:08:24 +0530

    Data Types

---
+++ Javascript/03_DataTypes/main.js
@@ -0,0 +1,37 @@
+// Data Types in JavaScript
+    // Number
+    // BigInt
+    // String
+    // Boolean
+    // Null
+    // Undefined
+    // Object
+
+
+// 1) Number
+let age = 2312345678903456789;
+let num1 = 10.5;
+console.log(typeof(age));
+console.log(age);
+
+// 2) String
+let name = "Deep";
+
+// 3) BigInt
+let bigIntValue = BigInt(1234567890123456789012345678901234567890);
+
+// 4) Boolean
+let isLoggedIn = true;
+let isLoggedOut = false;
+
+
+// 5) Null
+let container = null;
+
+
+// 6) Undefined
+let box;
+// box is declared but not initialized, so it is undefined
+
+
+// 7) Object

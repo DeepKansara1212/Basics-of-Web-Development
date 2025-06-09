63bee41a6a1b1f5bd82554072ef291f96f8a27ba
Deep Kansara <deepkansara1212@gmail.com>
2025-06-09 13:11:00 +0530

    String Methods

---
+++ Javascript/06_Strings/05_String-Methods.js
@@ -0,0 +1,25 @@
+let str = "Hello, I am learning Javascript."
+
+let str1 = "Hello"
+
+
+console.log(str.concat(" ", str1)) 
+
+// let str_len = str.charAt(10)
+// let str_len = str.at(-5)
+// let str_len = str.charCodeAt(10)
+// console.log(str_len);
+
+
+// let slice = str.slice(12, 20)
+// let slice = str.substr(12, 8)
+// let slice = str.slice(10)
+// console.log(slice);
+
+
+// let upper = str1.toUpperCase()
+// console.log(upper);
+// let lower = str1.toLowerCase()
+// console.log(lower);
+
+

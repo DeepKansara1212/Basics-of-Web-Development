8f908ef8b551439f05fb32f6d5a9008c93490923
Deep Kansara <deepkansara1212@gmail.com>
2025-06-11 12:49:32 +0530

    String Methods

--- Javascript/06_Strings/05_String-Methods.js
+++ Javascript/06_Strings/05_String-Methods.js
@@ -1,9 +1,40 @@
 let str = "Hello, I am learning Javascript."
 
-let str1 = "Hello"
+let str1 = "Hello World, world, world, World, World"
+
+let str2 = "Hello, World"
+
+console.log(str1.indexOf("World", 10))
+console.log(str1.lastIndexOf("World"))
+
+console.log(str1.search("World"));
 
 
-console.log(str.concat(" ", str1)) 
+// str_to_array = str2.split(",")
+// console.log(str_to_array);
+
+
+// replaced = str1.replace("World", "Javascript")
+// replaced = str1.replaceAll("World", "Javascript")
+// console.log(replaced);
+
+
+// console.log(str2.repeat(5));
+
+
+// let result_start = str2.padStart(5, "$")
+// let result_end = str2.padEnd(5, "@")
+// console.log(result_start);
+// console.log(result_end);
+
+
+// console.log(str1.trim());
+// console.log(str1.trimStart());
+// console.log(str1.trimEnd());
+
+
+
+// console.log(str.concat(" ", str1)) 
 
 // let str_len = str.charAt(10)
 // let str_len = str.at(-5)
@@ -21,5 +52,8 @@ console.log(str.concat(" ", str1))
 // console.log(upper);
 // let lower = str1.toLowerCase()
 // console.log(lower);
+
+
+
 
 

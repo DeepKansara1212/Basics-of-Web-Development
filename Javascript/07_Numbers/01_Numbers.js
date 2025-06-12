d819b781804a715cb0db7f827622a1358f921495
Deep Kansara <deepkansara1212@gmail.com>
2025-06-12 13:36:35 +0530

    Numbers

---
+++ Javascript/07_Numbers/01_Numbers.js
@@ -0,0 +1,27 @@
+number = BigInt(1234567890123456789)
+
+// console.log(number + BigInt(1));
+
+
+num1 = "100"
+num2 = "10"
+text = "hello"
+
+age1 = new Number(12) 
+age2 = 12
+
+name1 = new String("dfg")
+console.log(typeof(name1));
+
+
+console.log(age1 === age2);
+
+
+
+// console.log(num1 + num2);           // 10010
+// console.log(num1 - num2);           // 90
+// console.log(num1 * num2);           // 1000
+// console.log(num1 / num2);           // 10
+
+
+// console.log(num1 / text);
---
+++ Javascript/07_Numbers/02_Number-Methods.js
@@ -0,0 +1,34 @@
+number = 123456
+
+num_to_str = number.toString()
+
+// console.log(num_to_str);
+// console.log(typeof(num_to_str));
+
+
+// console.log(number.toExponential());
+// console.log(number.toExponential(2));
+// console.log(number.toExponential(4));
+
+
+num1 = 9.1383
+
+// console.log(num1.toFixed(2));   
+
+// console.log(num1.toPrecision(10));
+// console.log(num1.valueOf());
+
+
+let integer_num = "120.45"
+let int_to_float = parseFloat(integer_num)
+// console.log(int_to_float);
+
+
+console.log(Number("gh"));
+console.log(Number("123"));
+console.log(Number(false));
+
+
+
+// Falsy Value -> false, {}, [], "", 0
+

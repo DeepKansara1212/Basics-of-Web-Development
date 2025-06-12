ed5b45bcd1d17279f72bbe6434f62a36b1af611e
Deep Kansara <deepkansara1212@gmail.com>
2025-06-12 13:36:42 +0530

    Date

---
+++ Javascript/08_Date/01_Dates.js
@@ -0,0 +1,16 @@
+const d = new Date("October 13, 2014")
+
+// const d = new Date("October 13, 2014 11:13:00");
+
+console.log(d.getDate())
+console.log(d.getDay())
+console.log(d.getFullYear())
+console.log(d.getMonth())
+console.log(d.getTime())
+console.log(d.toDateString())
+console.log(d.toLocaleDateString()) 
+console.log(d.toString()) 
+
+// console.log(d);
+
+

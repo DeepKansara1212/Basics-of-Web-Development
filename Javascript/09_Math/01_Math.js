5a81bf2e098f38366ca6567372de1e6ee391a497
Deep Kansara <deepkansara1212@gmail.com>
2025-06-16 13:03:20 +0530

    Math

---
+++ Javascript/09_Math/01_Math.js
@@ -0,0 +1,25 @@
+// console.log(Math.E);
+// console.log(Math.PI);
+
+// console.log(Math.LN10);
+
+// console.log(Math.SQRT2);
+
+let a = 12.56
+
+console.log(Math.abs(-12.25));
+
+console.log(Math.ceil(a)); 
+
+console.log(Math.floor(a));
+
+console.log(Math.round(a));
+
+
+
+console.log(Math.max(12, 532, 20, 45, 78, 1000));
+console.log(Math.min(12, 532, 20, 45, 78, 1000));
+
+
+
+
---
+++ Javascript/09_Math/02_Random.js
@@ -0,0 +1,7 @@
+// console.log(Math.random());
+
+let number = (Math.random() * 10) + 1
+
+let int_number = Math.ceil(number)
+
+console.log(int_number); 

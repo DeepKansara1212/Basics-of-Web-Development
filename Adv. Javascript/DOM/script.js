9122d54dd308d8ef11afc3b2a08f10fbe8df530d
Deep Kansara <deepkansara1212@gmail.com>
2025-07-16 12:47:40 +0530

    DOM Manipulation

---
+++ Adv. Javascript/DOM/script.js
@@ -0,0 +1,13 @@
+// Element Selection
+const heading = document.getElementById('heading')
+const price = document.getElementsByClassName('price') 
+const para1 = document.querySelector('#para1')
+const para2 = document.querySelector('.para2') 
+
+
+// Content Updation
+heading.innerText = "Hello World !!!"
+para1.textContent = "Some Paragraph"
+para2.innerHTML = '<h1>Hello</h1>'
+
+console.log(para1.textContent); 

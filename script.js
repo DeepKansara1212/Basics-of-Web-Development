2f83aed15ec9041dd0ec3f3df2ae1859dcb5841d
Deep Kansara <deepkansara1212@gmail.com>
2025-08-12 13:08:31 +0530

    DOM Projects

---
+++ script.js
@@ -0,0 +1,25 @@
+// let num1 = 12
+// let num2 = 23 
+// let sum = num1 + num2
+// console.log(sum);
+
+// let num3 = 41
+// let num4 = 68
+// let sum1 = num3 + num4
+// console.log(sum1);
+
+
+function sum(num1, num2) {
+    sum = num1 + num2;
+    return sum;
+}
+
+let result = sum(10, 34)
+console.log(result);
+
+
+sum(10, 34)
+sum(10, 34)
+sum(10, 34)
+sum(10, 34)
+sum(10, 34)

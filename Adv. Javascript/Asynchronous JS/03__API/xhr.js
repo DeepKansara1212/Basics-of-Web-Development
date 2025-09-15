fa363cabcbda476a104a870ec551602ae3097aa7
Deep Kansara <deepkansara1212@gmail.com>
2025-09-15 13:20:27 +0530

    Add xhr

--- Adv. Javascript/Asynchronous JS/03__API/xhr.js
+++ Adv. Javascript/Asynchronous JS/03__API/xhr.js
@@ -3,3 +3,20 @@
 // const xhr = new XMLHttpRequest();
 
 // xhr.open('GET', 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}') 
+
+
+const xhr = new XMLHttpRequest();
+const user_url = 'https://randomuser.me/api/';
+
+xhr.open('GET', user_url);
+
+xhr.onload = function() {
+    if(xhr.readyState === 4) {
+        const data = JSON.parse(this.response);
+        // console.log(data);
+        const user = data.results[0].email;
+        console.log(user);
+    }
+}
+
+xhr.send();

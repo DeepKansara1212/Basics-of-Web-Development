e8994de2b5a0e7122a71667e44e09c14f1962045
Deep Kansara <deepkansara1212@gmail.com>
2025-10-06 13:22:56 +0530

    React CRA App

---
+++ React JS/01_cra-basics/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>I am learning ReactJS</p>
+    </>
+  );
+}
+
+export default App;

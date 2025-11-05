6237d45eb9445d39c879e93e0a9de73ac258780a
Deep Kansara <deepkansara1212@gmail.com>
2025-11-05 13:19:30 +0530

    Import Export

---
+++ React JS/05_Import-Export/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Gallary from './components/Gallary'
+
+const App = () => {
+  return (
+    <>
+      <Gallary />
+    </> 
+  )
+}
+
+export default App  
---
+++ React JS/05_Import-Export/src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Named Export
+export const Profile = () => {
+  return (
+    <>
+      <img
+        src="/profile.jpg"
+        alt="Katherine Johnson"
+        height={200}
+        width={200} 
+      />
+    </>
+  );
+};
